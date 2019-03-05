0f5932eb3d23742821c9845aeeed366688bbf7c6
mrCrawford <noreply>
2019-03-04 23:21:36 -0600

    Adding styling to project.  Might make a few more changes, but this could be submitted as is.

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,13 +1,15 @@
 //Javascript code for adding GIFs 
 
-var initialArray = ["Airplane", "Tall buildings", "Fire ants", "Guitars", "NHL", "Deep water", "Pizza", "Seventy-four", "circuit board", "euler"];
+var initialArray = ["Airplane", "Tall buildings", "Fire ants", "Guitars", "NHL", "Deep water", "Pizza", "Seventy-four", "circuit board", "gif"];
 var parentDiv = $('#button-container');
 var gifParent = $('#gif-container');
 
 var userInput = "";
 
 
-//Function for creating new buttons
+//This function creates new buttons.  This function takes a string variable as a parameter and adds this string as the text and as a 
+// data attribute to the button.
+
 function addButtons(searchValue){
 
     console.log(searchValue);
@@ -20,10 +22,13 @@ function addButtons(searchValue){
 }
 
 
+//This function performs an AJAX call.  It takes the user's input as a parameter and incorporates the user's input as a search in the 
+//AJAX url.  The first ten images are then returned and added to an <img> element in the DOM.  The animated GIF is added as a data 
+//attribute and is triggered by clicking the <img> element (see the animateImage function).
+
 function ajaxCall(userInput) {
 
     var searchQuery  = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=OU3PpuZNffiETCMCnwkQpqjXSm6szJYU&limit=10";
-
 
 
    $.ajax({
@@ -33,29 +38,35 @@ function ajaxCall(userInput) {
 
     var results = response.data;
 
-    console.log("Response: " + results);
-
     for(var i = 0; i < results.length; i++){
 
-    var imageURL = results[i].images.fixed_width_still.url;
+    var imageURL = results[i].images.original_still.url;
+    console.log(results[i].images);
 
     var gifImage = $('<img>');
+    var newDiv = $('<div>');
+    newDiv.addClass('divImage');
 
     gifImage.attr("src", imageURL);
     gifImage.attr("alt", userInput);
-    gifImage.attr("data-search", results[i].images.fixed_width.url);
+    gifImage.attr("data-search", results[i].images.original.url);
     gifImage.addClass('gifImage');
 
     var p = $('<p>').text("Rating: " + results[i].rating);
     p.append(gifImage);
 
-    gifParent.prepend(p);
+    newDiv.append(p);
+    newDiv.append(gifImage);
+    gifParent.prepend(newDiv);
     }
 
    });
 
 }
 
+
+//This function animates the <img> element that contains the gif.  The <img> element initially just shows a still photo of the gif.
+//However, when the user clicks the gif, the still is switched with the animated url which is stored in a data attribute (data-search).
 
 function animateImage(gifImage){
 
@@ -68,10 +79,16 @@ function animateImage(gifImage){
 
 $(document).ready(function(){
 
+
+//Initial creation of buttons from initialArray
+
 for(var i = 0; i< initialArray.length; i++){
+
     addButtons(initialArray[i]);
 
 }
+
+//Event handler to pull the gifs when the gif buttons are clicked
 
 $(document).on("click", "button.gifButton", function(event){
     var gifSearch = $(this).attr("data-search");
@@ -79,6 +96,7 @@ $(document).on("click", "button.gifButton", function(event){
 })
 
 
+//Event handler for the adding gif buttons
 
 $(document).on("click", "#gif-button", function(event){
     event.preventDefault();
@@ -86,6 +104,9 @@ $(document).on("click", "#gif-button", function(event){
     addButtons(userInput);
 
 })
+
+
+//Event handler to animate the gif image when it is clicked.
 
 $(document).on("click", ".gifImage", function(event){
     var gif = $(this);
